29372707419bb3c2dfa21b4e6eeac72278c3b2bf
blane thompson <blane1245@hotmail.co.uk>
2020-11-14 23:45:59 +0000

    Add dist to repository

---
+++ web/dist/precache-manifest.927e356a298d407cecaf33777e03a15a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f28866b634a9e6c931",
+    "url": "/css/app.51682945.css"
+  },
+  {
+    "revision": "704744b9249487bcaf85",
+    "url": "/css/chunk-vendors.d108840f.css"
+  },
+  {
+    "revision": "a3ccb4d8af8a3d7bee91b16445264f7d",
+    "url": "/img/brand.svg"
+  },
+  {
+    "revision": "7a23cf90309bc35a4af761aa9d06bc45",
+    "url": "/img/comfy.svg"
+  },
+  {
+    "revision": "56e2a7abd4ecfffec6a833076f1b2abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24f28866b634a9e6c931",
+    "url": "/js/app.52755b7c.js"
+  },
+  {
+    "revision": "704744b9249487bcaf85",
+    "url": "/js/chunk-vendors.93ff79c0.js"
+  },
+  {
+    "revision": "ff90a3854e44b8054213bbf552fb038b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ web/dist/precache-manifest.e3e086960098ff262427bd2bf7c09a94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0247c7b6a8efd7fdc8",
+    "url": "/css/app.51682945.css"
+  },
+  {
+    "revision": "e14a61528f80f39db327",
+    "url": "/css/chunk-vendors.d108840f.css"
+  },
+  {
+    "revision": "2a077f62fa0d45a9097585a30023092a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0247c7b6a8efd7fdc8",
+    "url": "/js/app-legacy.c8691ca1.js"
+  },
+  {
+    "revision": "e14a61528f80f39db327",
+    "url": "/js/chunk-vendors-legacy.1edcc7bd.js"
+  },
+  {
+    "revision": "ff90a3854e44b8054213bbf552fb038b",
+    "url": "/manifest.json"
+  }
+]);
