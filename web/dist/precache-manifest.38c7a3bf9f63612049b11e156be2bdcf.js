a8c7a4b79edf123f13cb2797a4bc51ddda93fc74
blane thompson <blane1245@hotmail.co.uk>
2021-05-05 17:57:22 +0100

    Update web build

--- web/dist/precache-manifest.38c7a3bf9f63612049b11e156be2bdcf.js
+++ web/dist/precache-manifest.38c7a3bf9f63612049b11e156be2bdcf.js
@@ -1,20 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<< HEAD:web/dist/precache-manifest.b49b073090ea0cb53a58ff5e51f27a22.js
     "revision": "ad05f9b4334c6f70c79a",
     "url": "/css/app.d39c747e.css"
   },
   {
     "revision": "f4b3d5d117feff587ae4",
     "url": "/css/chunk-vendors.9f09bd39.css"
-=======
-    "revision": "417659ae443b28618775",
-    "url": "/css/app.e07e46f4.css"
-  },
-  {
-    "revision": "8a19ef6a1ada059b9c38",
-    "url": "/css/chunk-vendors.6ecfd08d.css"
->>>>>>> 25ff753cefa8804f72eeb8d71770d781322659cf:web/dist/precache-manifest.38c7a3bf9f63612049b11e156be2bdcf.js
   },
   {
     "revision": "a3ccb4d8af8a3d7bee91b16445264f7d",
@@ -25,7 +16,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/comfy.svg"
   },
   {
-<<<<<<< HEAD:web/dist/precache-manifest.b49b073090ea0cb53a58ff5e51f27a22.js
     "revision": "2c497ecacef8d01006d9c2157d8c7c41",
     "url": "/index.html"
   },
@@ -36,18 +26,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f4b3d5d117feff587ae4",
     "url": "/js/chunk-vendors.d86f313d.js"
-=======
-    "revision": "4406b5c7435c6b43e5862ef9ec820c64",
-    "url": "/index.html"
-  },
-  {
-    "revision": "417659ae443b28618775",
-    "url": "/js/app.12d6d408.js"
-  },
-  {
-    "revision": "8a19ef6a1ada059b9c38",
-    "url": "/js/chunk-vendors.bd5c1c06.js"
->>>>>>> 25ff753cefa8804f72eeb8d71770d781322659cf:web/dist/precache-manifest.38c7a3bf9f63612049b11e156be2bdcf.js
   },
   {
     "revision": "ff90a3854e44b8054213bbf552fb038b",
