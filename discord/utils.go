ff35967add8b1dace92c4f955c8ee613f384ce48
Sanandan Sashikumar <cheesycod@outlook.com>
2024-05-10 09:01:46 +0000

    fix timestamp fixup

--- discord/utils.go
+++ discord/utils.go
@@ -9,17 +9,17 @@ import (
 
 type Timestamp string
 
-func (t *Timestamp) MarshalJSON() ([]byte, error) {
-	if t == nil {
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	if t == "" {
 		return []byte("null"), nil
 	}
 
-	if *t != "" {
-		if _, err := time.Parse(time.RFC3339, string(*t)); err != nil {
-			fmt.Printf("Timestamp is corrupted (is %v)\n", *t)
-			*t = ""
+	if t != "" {
+		if _, err := time.Parse(time.RFC3339, string(t)); err != nil {
+			fmt.Printf("Timestamp is corrupted (is %v)\n", t)
+			t = ""
 		}
 	}
 
-	return sandwichjson.Marshal(string(*t))
+	return sandwichjson.Marshal(string(t))
 }
