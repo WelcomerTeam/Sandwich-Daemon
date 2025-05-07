80c01f003c8c589cb024354b7fd6e2bd5b9fa00c
Blane Thompson <rock@welcomer.gg>
2025-05-07 05:52:19 +0000

    Initial commit

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ npm run build
 cd ..
 
 echo "Docker build and push"
-docker build --tag ghcr.io/welcomerteam/sandwich-daemon:latest .
-docker push ghcr.io/welcomerteam/sandwich-daemon:latest
+docker build --tag ghcr.io/welcomerteam/sandwich-daemon:next .
+docker push ghcr.io/welcomerteam/sandwich-daemon:next
--- web/src/api/user.js
+++
@@ -1,7 +0,0 @@
-import { fetch } from "./index";
-
-export default {
-  getUser(callback, errorCallback) {
-    fetch({ url: "/api/user" }, callback, errorCallback);
-  },
-};
