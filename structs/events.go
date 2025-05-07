3873df27b3322ca30380d3bfb9e2fb50be0c0ff0
Blane Thompson <rock@welcomer.gg>
2025-05-07 05:52:19 +0000

    Initial commit

--- structs/events.go
+++
@@ -1,7 +0,0 @@
-package structs
-
-const (
-	SandwichEventConfigurationReload    = "SW_CONFIGURATION_RELOAD"
-	SandwichEventShardStatusUpdate      = "SW_SHARD_STATUS_UPDATE"
-	SandwichEventShardGroupStatusUpdate = "SW_SHARD_GROUP_STATUS_UPDATE"
-)
