8bc0a128134509922f6fdc13e99038eabf5b7e62
Blane Thompson <rock@welcomer.gg>
2025-05-08 22:03:54 +0000

    Fixed null Transport on ProxyClients

--- client.go
+++ client.go
@@ -4,19 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 var UserAgent = fmt.Sprintf("Sandwich/%s (https://github.com/WelcomerTeam/Sandwich-Daemon)", Version)
 
 // NewProxyClient creates an HTTP client that redirects all requests through a specified host.
 // This is useful when using a proxy such as twilight or nirn.
-func NewProxyClient(client *http.Client, host url.URL) *http.Client {
+func NewProxyClient(client http.Client, host url.URL) *http.Client {
+	if client.Transport == nil {
+		client.Transport = http.DefaultTransport
+	}
+
 	client.Transport = &proxyTransport{
 		host:      host,
 		transport: client.Transport,
 	}
 
-	return client
+	return &client
 }
 
 type proxyTransport struct {
@@ -32,6 +37,10 @@ func (t *proxyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	proxyReq.URL.Host = t.host.Host
 	proxyReq.URL.Scheme = t.host.Scheme
 	proxyReq.Host = t.host.Host
+
+	if !strings.HasPrefix(proxyReq.URL.String(), "/api") {
+		proxyReq.URL.Path = "/api/v10" + proxyReq.URL.Path
+	}
 
 	proxyReq.Header.Set("User-Agent", UserAgent)
 
